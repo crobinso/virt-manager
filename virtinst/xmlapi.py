643e26d7752e49a4b6c391d60245d17c606ee5f1
Cole Robinson <crobinso@redhat.com>
2025-09-17 10:47:53 -0400

    XXX xmlapi add VIRTINST_XML_BACKEND selector
    Signed-off-by: Cole Robinson <crobinso@redhat.com>

--- virtinst/xmlapi.py
+++ virtinst/xmlapi.py
@@ -4,6 +4,17 @@
 # This work is licensed under the GNU GPLv2 or later.
 # See the COPYING file in the top-level directory.
 
+import os
+
+from .logger import log
 from .xmllibxml2 import Libxml2API
 
-XMLAPI = Libxml2API
+_backend = os.environ.get("VIRTINST_XML_BACKEND")
+log.debug("VIRTINST_XML_BACKEND=%s", _backend)
+
+if _backend == "libxml2":
+    XMLAPI = Libxml2API
+else:
+    # Default
+    XMLAPI = Libxml2API
+log.debug("Using XMLAPI=%s", XMLAPI)
